8dad551418c93360a5b81f472d5ce198119c6b28
ThemisMin <ThemisMin@gmail.com>
2024-11-04 10:21:15 +0800

    select 脚本优化

--- resources/views/form/select-script.blade.php
+++ resources/views/form/select-script.blade.php
@@ -24,6 +24,13 @@
     });
     @else
     $this.select2(configs);
+    $this.on("select2:select", function (evt) {
+        var element = evt.params.data.element;
+        var $element = $(element);
+        $element.detach();
+        $(this).append($element);
+        $(this).trigger("change");
+    });
     @endif
 
     {!! $cascadeScript !!}
